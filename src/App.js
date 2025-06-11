96c91e59fd68d27d267a8af38f8e710f2447a1c3
BethanyOrtiz0531 <bethanyortiz@costco.com>
2025-06-11 14:04:24 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PrivacyTrailScorecard from './PrivacyTrailScorecard';
+
+function App() {
+  return (
+    <div>
+      <PrivacyTrailScorecard />
+    </div>
+  );
+}
+
+export default App;
